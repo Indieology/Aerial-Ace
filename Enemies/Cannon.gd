d04cd9565ad18652d5e9442171e9e34d5983aa72
Indieology <contactindieology@gmail.com>
2022-08-03 15:49:48 -0500

    Initial Project

--- Enemies/Cannon.gd
+++ Enemies/Cannon.gd
@@ -1,44 +1,35 @@
-extends KinematicBody2D
+extends Area2D
 
-export var health : int = 3
-export var score : int = 75
+export var health : int = 1
+export var score : int = 25
 export var energy_dropped : int = 10
-export var verticalSpeed : int = 8
-#export var horizontalSpeed : int = 50
 
-onready var guns := $FiringPositions
-onready var fireTimer := $FireTimer
-onready var sprite := $AnimatedSprite
-onready var player : = get_parent().get_parent().get_parent().get_node("Player")
+
+onready var player = get_parent().get_parent().get_parent().get_node("Player")
+onready var gun := $FiringPosition
+onready var delay_timer := $DelayTimer
+onready var sprite := $Sprite
+
 
 onready var hurt_effect := preload("res://Effects/Hurt Effect.tscn")
 onready var explosion := preload("res://Effects/Explosion/Explosion.tscn")
 onready var bullet := preload("res://Projectiles/Bullet.tscn")
 
-
-#reference to the airplane node above, to check if it is still in scene
 onready var wr = weakref(player)
 
 func _ready():
 	#add_to_group("damageable")
 	randomize()
-	fireTimer.wait_time = rand_range(1,6)
-	fireTimer.start()
-
-func _process(delta):
-	if health == 2:
-		sprite.frame = 1
-	if health <= 1:
-		sprite.frame = 2
+	delay_timer.wait_time = rand_range(1,3)
+	delay_timer.start()
 
 func _physics_process(delta):
 	add_to_group("damageable")
-	#position.y += verticalSpeed * delta
-	if fireTimer.is_stopped() and global_position.y > 0:
+	if delay_timer.is_stopped() and global_position.y > 0:
 		fire()
 		randomize()
-		fireTimer.wait_time = rand_range(1,6)
-		fireTimer.start()
+		delay_timer.wait_time = rand_range(1,4)
+		delay_timer.start()
 
 func _on_Hurtbox_area_entered(area):
 	var this_hurt_effect = hurt_effect.instance()
@@ -56,6 +47,7 @@ func damage(amount: int):
 		var this_explosion := explosion.instance()
 		get_parent().add_child(this_explosion)
 		this_explosion.position = position
+		this_explosion.scale /= 2
 		#exit function if player no longer exists
 		if (!wr.get_ref()):
 			pass
@@ -65,11 +57,10 @@ func damage(amount: int):
 		queue_free()
 
 func fire():
-	for child in guns.get_children():
-			var this_bullet := bullet.instance()
-			get_parent().get_parent().get_parent().add_child(this_bullet)
-			this_bullet.position = child.global_position
-			this_bullet.direction = child.shoot_direction
+	var this_bullet := bullet.instance()
+	get_parent().get_parent().get_parent().add_child(this_bullet)
+	this_bullet.position = gun.global_position
+	this_bullet.direction = gun.shoot_direction
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
