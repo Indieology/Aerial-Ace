9d45cd30f0bda051765727e85a91ae9ee7fce9ad
Indieology <contactindieology@gmail.com>
2022-08-03 20:45:07 -0500

    Revert "Revert "Initial Project""
    This reverts commit 1396a3ad1a59e0c598f7ee06f6cf7c4003a15923.

--- Projectiles/Bullet.gd
+++ Projectiles/Bullet.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 export var speed: int = 500
+export var direction: Vector2 = Vector2.ZERO
 
 onready var bulletLifetime: Timer = $Lifetime
 
@@ -8,12 +9,16 @@ func _ready():
 	bulletLifetime.start()
 
 func _physics_process(delta):
-	position.y -= speed * delta
+	position += direction * speed * delta
 	if bulletLifetime.is_stopped():
 		queue_free()
 
 
 func _on_Bullet_area_entered(area):
-	if area.get_parent().is_in_group("damageable"):
+	if area.get_parent().is_in_group("damageable") and bulletLifetime.time_left < 1:
 		area.get_parent().damage(1)
 		queue_free()
+		
+	elif area.is_in_group("damageable") and bulletLifetime.time_left < 1:
+			area.damage(1)
+			queue_free()
